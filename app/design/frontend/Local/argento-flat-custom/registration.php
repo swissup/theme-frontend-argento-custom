6e321ea0755850c6a80a4bf9658c6b662647737d
Roman Chupryna <rommmmka@gmail.com>
2018-08-06 17:11:41 +0300

    Initial commit

---
+++ app/design/frontend/Local/argento-flat-custom/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Local/argento-flat-custom',
+    __DIR__
+);
