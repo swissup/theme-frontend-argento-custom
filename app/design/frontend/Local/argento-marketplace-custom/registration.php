216225b0015aa4492373e9637d3e089af0764c80
rommmka <rommmmka@gmail.com>
2023-06-07 13:58:16 +0300

    Added Marketplace Custom theme

---
+++ app/design/frontend/Local/argento-marketplace-custom/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Local/argento-marketplace-custom',
+    __DIR__
+);
